9aceefeab43cb64514c93e0b928a1d5a10a8fdae
bsh <mjbsh02@mju.ac.kr>
2026-06-10 21:50:28 +0900

    chore: Kakao Map API 키 설정

--- docs/design/js/config.js
+++ docs/design/js/config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
   SUPABASE_URL: 'https://porvghadkgpamnvbuyqu.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_cpvF4f7QZzxK16Q_-JNM5A_czghLSxK',
-  KAKAO_MAP_KEY: 'YOUR_KAKAO_MAP_KEY'
+  KAKAO_MAP_KEY: 'd1d53189f9fc723a64e060499a45f2a5'
 };
